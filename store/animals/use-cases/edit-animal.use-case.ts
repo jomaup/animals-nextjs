ba78d994e2fbedff035c1414ec78e439423a7cac
jomaup <maupas.joseph@yahoo.fr>
2022-05-18 15:35:22 +0200

    yes

--- store/animals/use-cases/edit-animal.use-case.ts
+++ store/animals/use-cases/edit-animal.use-case.ts
@@ -21,7 +21,7 @@ export const editAnimal =
     dispatch({ type: UPDATE_ANIMAL_REQUEST });
     try {
       const res: AxiosResponse<Animal> = await axios.put(
-        `https://animals-nest-js.herokuapp.com/${editedAnimal.id}`,
+        `https://animals-nest-js.herokuapp.com/animals/${editedAnimal.id}`,
         editedAnimal
       );
       handleEditAnimalSuccess(dispatch, res.data);
